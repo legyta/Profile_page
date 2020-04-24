84807b2669e83f59cc532ac480837f953846acf4
Ligita <legyta@gmail.com>
2020-04-24 10:58:13 +0200

    Updates

---
+++ precache-manifest.db47bc67e989c22b35e333e40ba729d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2558bb23c8759c60c40dbffd82a7bac6",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "830cc804abb38fa325a1",
+    "url": "/profile/static/css/main.65c1c878.chunk.css"
+  },
+  {
+    "revision": "3a691fe265a6a8268a2a",
+    "url": "/profile/static/js/2.4b28592e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/profile/static/js/2.4b28592e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "830cc804abb38fa325a1",
+    "url": "/profile/static/js/main.a1d59380.chunk.js"
+  },
+  {
+    "revision": "e749b72ed110487a6aec",
+    "url": "/profile/static/js/runtime-main.daaad5fa.js"
+  },
+  {
+    "revision": "dbb5ae20828f5b468da5e1f84c19b58a",
+    "url": "/profile/static/media/CV.dbb5ae20.pdf"
+  },
+  {
+    "revision": "7d58c61acb16d1850d58ee2bf5fcb9a8",
+    "url": "/profile/static/media/aboutPhoto.7d58c61a.jpg"
+  },
+  {
+    "revision": "ea3745acf242c2a07c1688d2ba7a0146",
+    "url": "/profile/static/media/photoGuide.ea3745ac.PNG"
+  },
+  {
+    "revision": "883d5b38c4017054cf77d7970a27f8f9",
+    "url": "/profile/static/media/sissy.883d5b38.PNG"
+  },
+  {
+    "revision": "28e08b9de418e137c75cb0745cba4556",
+    "url": "/profile/static/media/sloth.28e08b9d.PNG"
+  }
+]);
